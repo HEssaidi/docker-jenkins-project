330ab22e54484bae6f8ac87e309d8f91a5de9b03
HEssaidi <EssaidiHajar50@gmail.com>
2021-01-10 14:04:13 +0100

    add Dockerfile

--- src/main/java/com/example/dockerJ/helloworld.java
+++ src/main/java/com/example/dockerJ/helloworld.java
@@ -2,6 +2,6 @@ package com.example.dockerJ;
 
 public class helloworld {
 	public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello world second update");
     }
 }
